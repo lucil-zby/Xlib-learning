e42d463916fd7f91ff1854b95a05a8d8029807b4
lucil-zby <lucil_zby@hotmail.com>
2016-10-05 10:56:01 +0200

    Create sample1.c

---
+++ sample1.c
@@ -0,0 +1,24 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <stdlib.h>
+#define NIL (0)
+
+int main(){
+	// open display
+	Display *dpy = XOpenDisplay(NULL);
+
+	if (dpy != NULL)
+	{
+		// black and white color
+		unsigned long blackColor = BlackPixel(dpy, DefaultScreen(dpy));
+		unsigned long whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
+		/* create window*/
+		Window w = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0,0,200,200,0,blackColor, whiteColor);
+		/* map window */
+		XMapWindow(dpy, w);
+		/* flush */
+		XFlush(dpy);
+		sleep(10);
+		return 0;
+	}
+}
